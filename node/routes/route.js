59ecc17679238389c3415b55d439aeacf31c8eba
wy2008826 <760104178@qq.com>
2016-02-22 16:08:01 +0800

    add

--- node/routes/route.js
+++ node/routes/route.js
@@ -1,6 +1,16 @@
+module.id=12321;//module下面挂载的值可以被篡改
 
 module.exports=function(app){
 	app.get("/index",function(req,res,error){
-		res.render("index",{name:"习大大，哈哈哈"});
+		var data={
+			keys:Object.keys(module),
+			id:module.id,
+			filename:module.filename,
+			loaded:module.loaded,
+			parent:module.parent,
+			children:module.children
+
+		};
+		res.render("index",data);
 	});
 }
