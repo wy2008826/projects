d038619e494daada077d575cf47bd0ed542c63f1
wy2008826 <760104178@qq.com>
2016-06-12 19:32:00 +0800

    add

--- canvasGames/fish/public/js/main.js
+++ canvasGames/fish/public/js/main.js
@@ -1,9 +1,13 @@
 // 1.图片加载完成后绘制  同一个canvas上面绘制多张图片的绘制顺序问题
 // 2.不同的对象怎么去组织内部代码
-// 3.不同canvas之间对象的交互  以及碰撞检测问题
+// 3.不同canvas之间对象的交互  以及碰撞检测问题  对象和app之间的交互
 // 4.合理的布局应该是各种对象是分离的  app对象只负责将各种对象组织到一起，形成一个完整的业务逻辑
 
-//图片加载完成才能确定图片的尺寸 图片的缩放问题怎么搞？
+
+//app对象可以传递进所有的对象中 因为对象会实例化
+
+//图片加载完成才能确定图片的尺寸 图片的缩放问题怎么搞？ 对于只绘制一次的图片  使用onload  重复绘制的循环就好 
+
 
 //canvas常用的绘图api  绘制图片  线条  圆弧 。。。。
 function canvasUtil(){
@@ -81,7 +85,6 @@ Ane.prototype={
 			self.len[i]=baseLen+Math.random()*baseLen*0.3;
 			self.y[i]=self.c_h-self.len[i];
 		}
-		// console.log(self.x,self.y,self.len);
 	},
 	draw:function(){
 		var self=this;
@@ -170,6 +173,77 @@ Fruit.prototype={
 	}
 };
 
+// 鱼妈妈
+var Mom=function(o){
+	this.o=o;
+	this.ctx=o.ctx;
+	this.canvas=o.canvas;
+
+	this.body=new Image();//身子
+	this.eye=new Image();//眼睛
+	this.tail=new Image();//尾巴
+
+	this.init();
+};
+Mom.prototype={
+	init:function(){
+		var self=this;
+		this.body.src="./public/images/big.png";
+		this.eye.src="./public/images/bigEye0.png";
+		this.tail.src="./public/images/babyTail0.png";
+		this.x=self.canvas.width * 0.5;
+		this.y=self.canvas.height * 0.5;
+		this.angle=0;
+	},
+	draw:function(){
+		var self=this;
+		self.ctx.save();
+		self.ctx.translate(self.canvas.width * 0.5,self.canvas.height*0.5);
+		self.ctx.rotate(-Math.PI*0.25);
+		canvasUtil._drawImg(self.body,{
+			c_x:0 - self.body.width * 0.5,
+			c_y:0 - self.body.height * 0.5,
+			c_w:self.body.width,
+			c_h:self.body.height,
+			canvas:self.canvas,
+			ctx:self.ctx
+		});
+		canvasUtil._drawImg(self.eye,{
+			c_x:0 - self.eye.width * 0.5,
+			c_y:0 - self.eye.height * 0.5,
+			c_w:self.eye.width,
+			c_h:self.eye.height,
+			canvas:self.canvas,
+			ctx:self.ctx
+		});
+		
+		canvasUtil._drawImg(self.tail,{
+			c_x:30 - self.tail.width * 0.5,
+			c_y:0 - self.tail.height * 0.5,
+			c_w:self.tail.width,
+			c_h:self.tail.height,
+			canvas:self.canvas,
+			ctx:self.ctx
+		});
+		
+		self.ctx.restore();
+	},
+	updatePosition:function(x,y){
+		var self=this;
+		var disX=x-self.x;
+		var disY=y-self.y;
+		var dis=Math.sqrt(disX*disX+disY*disY);
+			
+		var now=new Date()*1;
+		var durTime=now-app.lastTime;
+
+		var angle=Math.atan(disY/disX);
+		console.log(angle);
+		// self.angle=angle*Math.PI-Math.PI*0.25;
+		console.log(self.angle);
+	}
+};
+
 
 
 (function(){
@@ -186,8 +260,9 @@ Fruit.prototype={
 		this.canvas1=document.getElementById("canvas1");
 		this.canvas2=document.getElementById("canvas2");
 		
-		this.init();
-		this.gameLoop();
+		this.init();//初始化各种信息
+		this.gameLoop();//动画循环
+		this.momAnimate();//大鱼的动画
 	};
 	App.prototype={
 		init:function(){
@@ -218,7 +293,12 @@ Fruit.prototype={
 				ctx:self.ctx1
 			});
 
-			
+
+			this.mom=new Mom({//鱼妈妈
+				canvas:self.canvas2,
+				ctx:self.ctx2
+			});
+
 		},
 		gameLoop:function(){
 			var self=this;
@@ -226,15 +306,25 @@ Fruit.prototype={
 			function loop(){
 				window.requestAnimationFrame(loop);
 				self.ctx1.clearRect(0,0,self.dprWidth,self.dprHeight);
+				self.ctx2.clearRect(0,0,self.dprWidth,self.dprHeight);
+
 				self.background.draw();//绘制背景图片
 				self.ane.draw();//绘制摆动的海葵
 				self.fruit.draw();//绘制果实
+				self.mom.draw();//绘制鱼妈妈
 
-				var now=new Date().getTime();
-				// console.log(now-self.lastTime);
 				self.lastTime=new Date().getTime();
+
 			}
 			loop();
+		},
+		momAnimate:function(){
+			var self=this;
+			self.canvas2.addEventListener("mousemove",function(e){
+				var inner_x=e.offsetX||e.layerX;
+				var inner_y=e.offsetY||e.layerY;
+				self.mom.updatePosition(inner_x,inner_y);//更新鱼的位置坐标
+			});
 		}
 	};
 
