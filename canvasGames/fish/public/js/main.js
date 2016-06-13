04afe0c2b36b3b4414c7dfb4d818a6d6371fb681
wy2008826 <760104178@qq.com>
2016-06-13 22:22:22 +0800

    add

--- canvasGames/fish/public/js/main.js
+++ canvasGames/fish/public/js/main.js
@@ -5,7 +5,7 @@
 
 //鼠标交互的坐标和实际对象的坐标位置会有清晰度的差别  要注意
 
-//app对象可以传递进所有的对象中 因为对象会实例化
+//app对象怎么传递到其他对象中？
 
 //图片加载完成才能确定图片的尺寸 图片的缩放问题怎么搞？ 对于只绘制一次的图片  使用onload  重复绘制的循环就好 
 
@@ -36,6 +36,17 @@ function canvasUtil(){
 		ctx.lineTo();
 		ctx.lineWidth=12;
 		ctx.restore();
+	},
+	this._lerpPosition=function(aim,cur,deta){
+		return {
+			x:cur.x+(aim.x-cur.x)*deta,
+			y:cur.y+(aim.y-cur.y)*deta
+		}
+	},
+	this._calDistance=function(aim,cur){
+		var disX=aim.x-cur.x;
+		var disY=aim.y-cur.y;
+		return Math.sqrt(disX*disX+disY*disY);
 	}
 };
 var canvasUtil=new canvasUtil();
@@ -66,7 +77,7 @@ Background.prototype={
 };
 
 
-//海葵对象
+//海葵
 var Ane=function(option){//海葵对象 canvas ctx
 	var o=this.o=option;
 	this.canvas=o.canvas;
@@ -112,7 +123,7 @@ Ane.prototype={
 };
 
 
-//果实对象
+//果实
 var Fruit=function(o){
 	this.num=30;//果实数量
 	this.canvas=o.canvas;
@@ -161,21 +172,29 @@ Fruit.prototype={
 			}
 			
 			if(fruit.y<-20){//果实死亡
+				fruit.alive=false;
 				self.born(i);
 			}
-			canvasUtil._drawImg(fruitImg,{
-				c_x:fruit.x-fruit.size*0.5,
-				c_y:fruit.y-fruit.size*0.5,
-				c_w:fruit.size,
-				c_h:fruit.size,
-				ctx:self.ctx,
-				canvas:self.canvas
-			});
+			if(fruit.alive){//活着
+				canvasUtil._drawImg(fruitImg,{
+					c_x:fruit.x-fruit.size*0.5,
+					c_y:fruit.y-fruit.size*0.5,
+					c_w:fruit.size,
+					c_h:fruit.size,
+					ctx:self.ctx,
+					canvas:self.canvas
+				});
+			}
+			
 		}
 	},
 	born:function(id){
 		var self=this;
 		self.fruit[id]=self.createFruit(id);
+	},
+	die:function(id){
+		var self=this;
+		self.fruit[id].alive=false;
 	}
 };
 
@@ -190,13 +209,14 @@ var Mom=function(o){
 	this.tail=new Image();//尾巴
 
 	this.init();
+	this.animate();
 };
 Mom.prototype={
 	init:function(){
 		var self=this;
 		this.body.src="./public/images/big.png";
 		this.eye.src="./public/images/bigEye0.png";
-		this.tail.src="./public/images/babyTail0.png";
+		this.tail.src="./public/images/bigTail0.png";
 
 		var canW=self.canvas.width;
 		var canH=self.canvas.height;
@@ -239,11 +259,16 @@ Mom.prototype={
 			ctx:self.ctx
 		});
 		
-		var deta=0.06;
-		self.x=self.x+(self.mouseX-self.x)*deta;
-		self.y=self.y+(self.mouseY-self.y)*deta;
-		
+		var newPos=canvasUtil._lerpPosition(
+			{x:self.mouseX,y:self.mouseY},
+			{x:self.x,y:self.y},
+			0.06
+		);
+		self.x=newPos.x;
+		self.y=newPos.y;
+		self.o.context.baby.updatePosition(self.x,self.y);//更改小鱼的位置
 		self.ctx.restore();
+		self.collision();//碰撞检测 应该是只有鱼主动移动的时候  才会吃鱼  被动撞击不去吃
 	},
 	updatePosition:function(x,y){
 		var self=this;
@@ -258,6 +283,119 @@ Mom.prototype={
 		self.mouseX=x;
 		self.mouseY=y;
 		
+	},
+	animate:function(){
+		var self=this;
+		self.canvas.addEventListener("mousemove",function(e){
+			var inner_x=(e.offsetX||e.layerX)*self.o.context.dpr;
+			var inner_y=(e.offsetY||e.layerY)*self.o.context.dpr;
+			self.updatePosition(inner_x,inner_y);//更新鱼的位置坐标
+		});
+	},
+	collision:function(){
+		var self=this;
+		var fruitO=self.o.context.fruit;
+		
+		
+		for(var i=0;i<fruitO.fruit.length;i++){
+			var fruitItem=fruitO.fruit[i];
+			var aim_pos={x:self.x,y:self.y};
+			var cur_pos={x:fruitItem.x,y:fruitItem.y};
+			if(canvasUtil._calDistance(aim_pos,cur_pos)<self.body.width*0.5){
+				fruitItem.alive=false;
+			}
+		}
+	}
+};
+
+//小鱼
+var Baby=function(o){
+
+	this.o=o;
+	this.ctx=o.ctx;
+	this.canvas=o.canvas;
+
+	this.body=new Image();//身子
+	this.eye=new Image();//眼睛
+	this.tail=new Image();//尾巴
+
+	this.init();
+	this.animate();
+};
+
+Baby.prototype={
+	init:function(){
+		var self=this;
+		this.body.src="./public/images/baby.png";
+		this.eye.src="./public/images/babyEye0.png";
+		this.tail.src="./public/images/babyTail0.png";
+
+		var canW=self.canvas.width;
+		var canH=self.canvas.height;
+
+		this.x=canW * 0.5 - 50;//鱼的默认起始坐标
+		this.y=canH * 0.5 - 50;
+		this.aimX=canW * 0.5- 50;//鼠标的默认起始坐标
+		this.aimY=canH * 0.5- 50;
+		this.angle=0;//鱼的默认起始角度
+	},
+	draw:function(){
+		var self=this;
+		self.ctx.save();
+		self.ctx.translate(self.x,self.y);//当一个对象有多个部位组成的时候  位移通过translate移动
+		self.ctx.rotate(self.angle);
+
+		canvasUtil._drawImg(self.body,{
+			c_x:0- self.body.width * 0.5,
+			c_y:0 - self.body.height * 0.5,
+			c_w:self.body.width,
+			c_h:self.body.height,
+			canvas:self.canvas,
+			ctx:self.ctx
+		});
+		canvasUtil._drawImg(self.eye,{
+			c_x:0 - self.eye.width * 0.5,
+			c_y:0 - self.eye.height * 0.5,
+			c_w:self.eye.width,
+			c_h:self.eye.height,
+			canvas:self.canvas,
+			ctx:self.ctx
+		});
+		
+		canvasUtil._drawImg(self.tail,{
+			c_x:26 - self.tail.width * 0.5,
+			c_y:0 - self.tail.height * 0.5,
+			c_w:self.tail.width,
+			c_h:self.tail.height,
+			canvas:self.canvas,
+			ctx:self.ctx
+		});
+		
+		var newPos=canvasUtil._lerpPosition(
+			{x:self.aimX,y:self.aimY},
+			{x:self.x,y:self.y},
+			0.01
+		);
+		self.x=newPos.x;
+		self.y=newPos.y;
+
+		self.ctx.restore();
+	},
+	updatePosition:function(x,y){
+		var self=this;
+		var disX=x-self.x;
+		var disY=y-self.y;
+		var dis=Math.sqrt(disX*disX+disY*disY);
+		
+		var now=new Date()*1;
+		// var durTime=now-app.lastTime;
+		var angle=Math.atan(disY/disX);
+		self.angle=disX>=0?angle+Math.PI:angle;
+		self.aimX=x;
+		self.aimY=y;
+	},
+	animate:function(){
+
 	}
 };
 
@@ -279,7 +417,7 @@ Mom.prototype={
 		
 		this.init();//初始化各种信息
 		this.gameLoop();//动画循环
-		this.momAnimate();//大鱼的动画
+		
 	};
 	App.prototype={
 		init:function(){
@@ -297,25 +435,33 @@ Mom.prototype={
 
 			this.background=new Background({//背景
 				canvas:self.canvas1,
-				ctx:self.ctx1
+				ctx:self.ctx1,
+				context:self
 			});
 
 			this.ane=new Ane({//海葵
 				canvas:self.canvas1,
-				ctx:self.ctx1
+				ctx:self.ctx1,
+				context:self
 			});
 
 			this.fruit=new Fruit({//果实
 				canvas:self.canvas1,
-				ctx:self.ctx1
+				ctx:self.ctx1,
+				context:self
 			});
-
 
 			this.mom=new Mom({//鱼妈妈
 				canvas:self.canvas2,
-				ctx:self.ctx2
+				ctx:self.ctx2,
+				context:self
 			});
 
+			this.baby=new Baby({//小鱼
+				canvas:self.canvas2,
+				ctx:self.ctx2,
+				context:self
+			});
 		},
 		gameLoop:function(){
 			var self=this;
@@ -329,19 +475,13 @@ Mom.prototype={
 				self.ane.draw();//绘制摆动的海葵
 				self.fruit.draw();//绘制果实
 				self.mom.draw();//绘制鱼妈妈
+				self.baby.draw();//绘制小鱼
+
 
 				self.lastTime=new Date().getTime();
 
 			}
 			loop();
-		},
-		momAnimate:function(){
-			var self=this;
-			self.canvas2.addEventListener("mousemove",function(e){
-				var inner_x=(e.offsetX||e.layerX)*self.dpr;
-				var inner_y=(e.offsetY||e.layerY)*self.dpr;
-				self.mom.updatePosition(inner_x,inner_y);//更新鱼的位置坐标
-			});
 		}
 	};
 
