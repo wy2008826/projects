03053a553685df62255d0c44e52ce2be299d5b89
wy2008826 <760104178@qq.com>
2017-03-06 15:22:59 +0800

    change

--- onlineProject/routes/route.js
+++ onlineProject/routes/route.js
@@ -2,18 +2,18 @@
 
 module.exports=function(app){
 	//首页
-	app.get("/index",function(req,res,next){
-		var data={
-			title:"首页"
-		};
-		res.render("index",data);
-		
+	app.get("/",function(req,res,next){
+		res.render("index");
+	});
+
+	app.get("/index.html",function(req,res,next){
+		res.render("index");
 	});
 
 	//个人简历
-	app.get("/resume",function(req,res,next){
+	app.get("/resume.html",function(req,res,next){
 		res.render("resume");
 	});
 
-	
+
 }
