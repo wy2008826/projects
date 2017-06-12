74af218b86bfd2aa3a773b51163c73c47c94cb70
wy2008826 <wy1479285>
2017-06-12 21:44:00 +0800

    add

--- stocks/practice/watchAndSendNowTBeforeClose.js
+++ stocks/practice/watchAndSendNowTBeforeClose.js
@@ -9,9 +9,9 @@ module.exports=function(){
 		var hours=now.getHours();
 		var minutes=now.getMinutes();
 
-
+		console.log(hours,minutes)
 		if(day!=6||day!=0){//非周末
-			if(hours==21&&minutes==40){
+			if(hours==21&&minutes==46){
 				crawAllSlotsAndSearchOneDayT(undefined,undefined,"email");
 			}
 		}
