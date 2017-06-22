ea01a73a95fa8f9e6614ef388ea8c8d4e94a68f7
wy2008826 <wy1479285>
2017-06-22 20:04:02 +0800

    add

--- stocks/routes/controllers/crawHistoryDataAll.js
+++ stocks/routes/controllers/crawHistoryDataAll.js
@@ -11,28 +11,35 @@ let timeFormat=require("../utils/getYMDHMS.js");
 //从数据库中查找股票的历史数据，按照历史数据最近一次的日期进行历史数据查询  完善数据库历史数据记录
 module.exports=async function(){
 	let begain=new Date();
-	StockModel.find({},async function(err,stocks){
-		for(let i=0;i<stocks.length;i++){
-			
-			let stock=stocks[i];
-			let code=stock.code;
-			let historyData=stock.historyData;
-			let start;
-			if(historyData){
-				start=historyData.end;
-				if(getDay(start)==getDay(new Date())){
-					console.log("code historyData is fresh to now! skiped....")
-					continue;
+	return new Promise(function(resolve,reject){
+		StockModel.find({},async function(err,stocks){
+			if(err){
+				reject(err);
+			}else{
+				for(let i=0;i<stocks.length;i++){
+					
+					let stock=stocks[i];
+					let code=stock.code;
+					let historyData=stock.historyData;
+					let start;
+					if(historyData){
+						start=historyData.end;
+						if(getDay(start)==getDay(new Date())){
+							console.log("code historyData is fresh to now! skiped....")
+							continue;
+						}
+					}
+					
+					await crawHistoryDataOne(code,start);
+				
 				}
+				let end=new Date();
+				let minutes=( (end-begain) / (1000 * 60 ) );
+				console.log(`loaded all historyData 😊 !!! 共耗时 ${minutes} 分钟`)
 			}
-			
-			await crawHistoryDataOne(code,start);
-		
-		}
-		let end=new Date();
-		let minutes=( (end-begain) / (1000 * 60 ) );
-		console.log(`loaded all historyData 😊 !!! 共耗时 ${minutes} 分钟`)
+		});
 	});
+	
 }
 
 
